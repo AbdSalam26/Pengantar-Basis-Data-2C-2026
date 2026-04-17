805626062c6b1cc54f8a217103de3f1289bcd787
AbdSalam26 <ilamgraterr@gmail.com>
2026-04-18 00:11:10 +0700

    250441100122_ABD SALAM ALI_MODUL1_ASPRAKKAKHEEMA

---
+++ modul-1/250441100122_AbdSalamAli_asprakkakheema/soal3.sql
@@ -0,0 +1,6 @@
+CREATE TABLE transaksi_harian (
+    id_transaksi INT,
+    jumlah DECIMAL(10,2)
+);
+DROP TABLE transaksi_harian;
+TRUNCATE TABLE transaksi_harian;
---
+++ modul-1/250441100122_AbdSalamAli_asprakkakheema/soal4.sql
@@ -0,0 +1,5 @@
+CREATE TABLE umpan_balik (
+    id_komentar INT PRIMARY KEY,
+    nama_pengguna VARCHAR(100),
+    isi_komentar TEXT
+);
