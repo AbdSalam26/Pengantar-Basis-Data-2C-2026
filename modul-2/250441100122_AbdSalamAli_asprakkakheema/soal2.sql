0c4caa0bef5254a9673d6f1cf5d84ea37ef90069
AbdSalam26 <ilamgraterr@gmail.com>
2026-04-18 00:14:40 +0700

    250441100122_ABD SALAM ALI_MODUL2_ASPRAKKAKHEEMA

---
+++ modul-2/250441100122_AbdSalamAli_asprakkakheema/soal2.sql
@@ -0,0 +1,10 @@
+INSERT INTO dokter VALUES
+(1, 'Dr. ali', 'Umum'),
+(2, 'Dr. Budi', 'Gigi');
+INSERT INTO pasien VALUES
+(1, 'upin', '2026-02-26','081'),
+(2, 'ipin', '2026-02-26','082');
+(1, 1, 1, '2026-02-01', 'Demam'),
+(2, 2, 2, '2026-02-02', 'Sakit gigi');
+INSERT INTO rekam_medis VALUES
+(3, 3, 3, '2026-04-03', 'Flu');
---
+++ modul-2/250441100122_AbdSalamAli_asprakkakheema/soal3.sql
@@ -0,0 +1,8 @@
+UPDATE pasien
+SET no_telepon = '081987628362'
+WHERE id_pasien = 1;
+UPDATE dokter
+SET spesialisasi = 'hewan'
+WHERE id_dokter = 1;
+DELETE FROM rekam_medis
+WHERE id_rekam = 2;
